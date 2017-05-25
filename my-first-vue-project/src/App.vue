b2b22de21a1c49184d4a083b8ad2f8beb52fe846
seelsee <634219761@qq.com>
2017-05-25 10:36:22 +0800

    no l

---
+++ my-first-vue-project/src/App.vue
@@ -0,0 +1,55 @@
+  <template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class='{finished: item.isFinished}' v-on:click="toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      title:'this is todo list',
+      items: [
+        
+      ],
+      newItem: ''
+    }
+  },
+  methods: {
+    toggleFinish: function (item) {
+     item.isFinished = !item.isFinished;
+    },
+    addNew: function() {
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      // console.log(this.newItem);
+      this.newItem = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.finished {
+  text-decoration: underline;
+}
+</style>
